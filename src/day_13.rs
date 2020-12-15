7686903696a795192963565064b16e896a1abaee
Gabriel Restori Soares <grsoares21@gmail.com>
2020-12-15 19:33:46 +0100

    Implemented second solution for day 13

--- src/day_13.rs
+++ src/day_13.rs
@@ -26,14 +26,74 @@ fn first_challenge(input: &String) {
     );
 }
 
-/*fn second_challenge(input: &String) {
-    println!("Second challenge result: {}", 0);
-}*/
+/**
+ * Chinese remainder theorem algorithm extracted from:
+ * https://rosettacode.org/wiki/Chinese_remainder_theorem#Rust
+ *
+*/
+fn extended_greatest_common_divisor(a: i64, b: i64) -> (i64, i64, i64) {
+    if a == 0 {
+        (b, 0, 1)
+    } else {
+        let (g, x, y) = extended_greatest_common_divisor(b % a, a);
+        (g, y - (b / a) * x, x)
+    }
+}
+fn modular_inverse(x: i64, n: i64) -> Option<i64> {
+    let (g, x, _) = extended_greatest_common_divisor(x, n);
+    if g == 1 {
+        Some((x % n + n) % n)
+    } else {
+        None
+    }
+}
+fn chinese_remainder(residues: &[i64], modulii: &[i64]) -> Option<i64> {
+    let prod = modulii.iter().product::<i64>();
+    let mut sum = 0;
+    for (&residue, &modulus) in residues.iter().zip(modulii) {
+        let p = prod / modulus;
+        sum += residue * modular_inverse(p, modulus)? * p
+    }
+    Some(sum % prod)
+}
+
+fn second_challenge(input: &String) {
+    let lines: Vec<&str> = input.split('\n').collect();
+    let buses_and_indexes: Vec<(usize, i32)> = lines[1]
+        .split(',')
+        .enumerate()
+        .filter(|(_, bus_id)| *bus_id != "x")
+        .map(|(index, bus_id)| (index, bus_id.parse::<i32>().unwrap()))
+        .collect();
+
+    let mut max_index = i32::MIN;
+    for (index, _) in &buses_and_indexes {
+        if *index as i32 > max_index {
+            max_index = *index as i32;
+        }
+    }
+
+    let residues: Vec<i64> = (&buses_and_indexes)
+        .iter()
+        .map(|(index, _)| max_index as i64 - *index as i64)
+        .collect();
+    let modulii: Vec<i64> = (&buses_and_indexes)
+        .iter()
+        .map(|(_, bus_id)| *bus_id as i64)
+        .collect();
+
+    let value = chinese_remainder(&residues, &modulii);
+
+    println!(
+        "Second challenge result: {}",
+        value.unwrap() - max_index as i64
+    );
+}
 
 pub fn day_13() {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input);
 
     first_challenge(&input);
-    //second_challenge(&input);
+    second_challenge(&input);
 }
